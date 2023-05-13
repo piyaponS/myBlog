426041bac9ebaa2b1b1f437f5773e1fde82aec09
piyaponSae <piyapon.sae@gmail.com>
2023-05-13 22:37:50 +0700

    Finish modal function

--- src/components/ModalMessage.js
+++ src/components/ModalMessage.js
@@ -1,6 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import classes from "./ModalMessage.module.css";
+import Form from "react-bootstrap/Form";
 
 function ModalMessage(props) {
   return (
@@ -10,16 +11,45 @@ function ModalMessage(props) {
       </Modal.Header>
 
       <Modal.Body>
-        <input style={{ width: "100%" }} autoFocus></input>
-      </Modal.Body>
-      <Modal.Body>
-        <input style={{ width: "100%" }}></input>
-      </Modal.Body>
-      <Modal.Body>
-        <input style={{ width: "100%" }}></input>
-      </Modal.Body>
-      <Modal.Body>
-        <input style={{ width: "100%" }}></input>
+        <Form>
+          <Form.Group>
+            <Form.Label>Article Title</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Article Title"
+              name="name"
+              autoFocus
+              autoComplete="off"
+              className="mb-2"
+            />
+            <Form.Label>About</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="What's this article about?"
+              name="name"
+              autoComplete="off"
+              className="mb-2"
+            />
+            <Form.Label>Article Content</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={4}
+              type="text"
+              placeholder="Write your article"
+              name="name"
+              autoComplete="off"
+              className="mb-2"
+            />
+            <Form.Label>Tags</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter tags"
+              name="name"
+              autoComplete="off"
+              className="mb-2"
+            />
+          </Form.Group>
+        </Form>
       </Modal.Body>
 
       <Modal.Footer>
